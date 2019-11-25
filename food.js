82613b9dcb869da63f89bbccc5b93800cf20a4b7
tago3904 <44854214+tago3904@users.noreply.github.com>
2019-11-24 18:59:34 -0700

    Add files via upload

---
+++ food.js
@@ -0,0 +1,212 @@
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://webknox-recipes.p.rapidapi.com/recipes/search");
+
+req.query({
+	"number": "100",
+	"cuisine": "italian",
+	"intolerances": userInput,
+	"query": "italian"
+});
+
+req.headers({
+	"x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+	"x-rapidapi-key": "eea6519e62mshf725c5dbfac1388p1fcfdbjsn057c577d43dd"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://webknox-recipes.p.rapidapi.com/recipes/search");
+
+req.query({
+	"number": "100",
+	"cuisine": "mexican",
+	"intolerances": userInput,
+	"query": "mexican"
+});
+
+req.headers({
+	"x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+	"x-rapidapi-key": "eea6519e62mshf725c5dbfac1388p1fcfdbjsn057c577d43dd"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://webknox-recipes.p.rapidapi.com/recipes/search");
+
+req.query({
+	"number": "100",
+	"cuisine": "middle eastern",
+	"intolerances": userInput,
+	"query": "middle eastern"
+});
+
+req.headers({
+	"x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+	"x-rapidapi-key": "eea6519e62mshf725c5dbfac1388p1fcfdbjsn057c577d43dd"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
+
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://webknox-recipes.p.rapidapi.com/recipes/search");
+
+req.query({
+	"number": "100",
+	"cuisine": "asian",
+	"intolerances": userInput,
+	"query": "asian"
+});
+
+req.headers({
+	"x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+	"x-rapidapi-key": "eea6519e62mshf725c5dbfac1388p1fcfdbjsn057c577d43dd"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://webknox-recipes.p.rapidapi.com/recipes/search");
+
+req.query({
+	"number": "100",
+	"cuisine": "french",
+	"intolerances": userInput,
+	"query": "french"
+});
+
+req.headers({
+	"x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+	"x-rapidapi-key": "eea6519e62mshf725c5dbfac1388p1fcfdbjsn057c577d43dd"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://webknox-recipes.p.rapidapi.com/recipes/search");
+
+req.query({
+	"number": "100",
+	"cuisine": "american",
+	"intolerances": userInput,
+	"query": "american"
+});
+
+req.headers({
+	"x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+	"x-rapidapi-key": "eea6519e62mshf725c5dbfac1388p1fcfdbjsn057c577d43dd"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://webknox-recipes.p.rapidapi.com/recipes/search");
+
+req.query({
+	"number": "100",
+	"cuisine": "greek",
+	"intolerances": userInput,
+	"query": "greek"
+});
+
+req.headers({
+	"x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+	"x-rapidapi-key": "eea6519e62mshf725c5dbfac1388p1fcfdbjsn057c577d43dd"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://webknox-recipes.p.rapidapi.com/recipes/search");
+
+req.query({
+	"number": "100",
+	"cuisine": "indian",
+	"intolerances": userInput,
+	"query": "indian"
+});
+
+req.headers({
+	"x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+	"x-rapidapi-key": "eea6519e62mshf725c5dbfac1388p1fcfdbjsn057c577d43dd"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
+
+var unirest = require("unirest");
+
+var req = unirest("GET", "https://webknox-recipes.p.rapidapi.com/recipes/search");
+
+req.query({
+	"number": "100",
+	"cuisine": "african",
+	"intolerances": userInput,
+	"query": "african"
+});
+
+req.headers({
+	"x-rapidapi-host": "webknox-recipes.p.rapidapi.com",
+	"x-rapidapi-key": "eea6519e62mshf725c5dbfac1388p1fcfdbjsn057c577d43dd"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body);
+});
