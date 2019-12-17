28448c9900c1a4e9bd6cae053b94a07c9fe80ce2
ryke7507 <49416723+ryke7507@users.noreply.github.com>
2019-12-16 20:52:19 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { Pool, Client } = require('pg'); //package for handling postgress
-const keys = require('./keys');  // get spoonacular api key
+//const keys = require('./keys');  // get spoonacular api key
 const pgPromise = require('pg-promise');
 const nodeFetch = require('node-fetch');
 var passport = require("passport");
